db60a53939c31bb11e1ae7b5d7f605307783b64a
Christy Hanson <chrsthnsn@gmail.com>
2023-06-19 14:49:47 -0600

    working on updating employee roles in index.js

--- helpers/sqlRequest.js
+++ helpers/sqlRequest.js
@@ -46,7 +46,7 @@ function displayEmployees() {
     return new Promise((resolve, reject) => {
         // query using SQL
         // Specificity
-        db.query(`SELECT * FROM employee`, (error, data) => {
+        db.query(`SELECT employee.id AS ID, CONCAT (employee.first_name,' ',employee.last_name) AS Name, role.title AS Role, role.salary AS Salary, CONCAT(manager.first_name,' ',manager.last_name) AS Manager FROM employee JOIN role ON employee.role_id=role.id JOIN department ON role.department_id=department.id LEFT JOIN employee manager ON manager.id=employee.manager_id`, (error, data) => {
             if (error) {
                 reject(error);
             } else {
@@ -57,5 +57,119 @@ function displayEmployees() {
     });
 }
 
+// get db information as array
+function getEmployeeNames() {
+    return new Promise((resolve, reject) => {
+        // SQL - ARRAYAGG turns it into array
+        // rowsAsArray is a property set to true
+        db.query({ sql: `SELECT JSON_ARRAYAGG(CONCAT(first_name, ' ', last_name)) FROM employee;`, rowsAsArray: true }, (error, result) => {
+            if (error) {
+                reject(error);
+            } else {
+                var names = [];
+                result[0][0].forEach(element => {
+                    names.push(element)
+                });
+                resolve(names);
+            }
+        })
+    })
+};
+
+function getRoleTitles() {
+    return new Promise((resolve, reject) => {
+        // SQL - ARRAYAGG turns it into array
+        // rowsAsArray is a property set to true
+        db.query({ sql: `SELECT JSON_ARRAYAGG(title) FROM role;`, rowsAsArray: true }, (error, result) => {
+            if (error) {
+                reject(error);
+            } else {
+                var titles = [];
+                result[0][0].forEach(element => {
+                    titles.push(element)
+                });
+                resolve(titles);
+            }
+        })
+    })
+};
+
+function getDepartmentNames() {
+    // Promise corresponds to .thens in index.js
+    return new Promise((resolve, reject) => {
+        // SQL - ARRAYAGG turns it into array
+        db.query({ sql: `SELECT JSON_ARRAYAGG(dept_name) FROM department;`, rowsAsArray: true }, (error, result) => {
+            if (error) {
+                reject(error);
+            } else {
+                var deptNames = [];
+                result[0][0].forEach(element => {
+                    deptNames.push(element)
+                });
+                resolve(deptNames);
+            }
+        })
+    })
+};
+
+//  Create new function for addEmployee, which will come from the index.js file inside displayEmpQuestions
+function addEmployee(first_name, last_name, title, manager_name) {
+    return new Promise((resolve, reject) => {
+        // SQL - ARRAYAGG turns it into array
+        // convert the title to an ID.
+        db.query(`SELECT id FROM role WHERE title='${title}';`, (error, result) => {
+            if (error) {
+                reject(error);
+            } else {
+                // insert into role id in the db
+                var role_id = result[0].id;
+                db.query(`SELECT id FROM employee WHERE CONCAT(employee.first_name,' ',employee.last_name)='${manager_name}';`, (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        // insert into role id in the db
+                        var manager_id = result[0].id;
+                        db.query(`INSERT INTO employee(first_name,last_name,role_id, manager_id) VALUE("${first_name}","${last_name}", ${role_id}, ${manager_id})`, (error, result) => {
+                            if (error) {
+                                reject(error);
+                            } else {
+                                console.log(`Added ${first_name} ${last_name} to employee db`);
+                                resolve();
+
+                            }
+                        })
+                    }
+                })
+
+            }
+        })
+    })
+};
+
+// add ROLE, convert dept name to and id 
+function addRole(title, salary, department_name) {
+    return new Promise((resolve, reject) => {
+        // SQL - ARRAYAGG turns it into array
+        // convert the title to an ID.
+        db.query(`SELECT id FROM department WHERE dept_name='${department_name}';`, (error, result) => {
+            if (error) {
+                reject(error);
+            } else {
+                // insert into role id in the db
+                var dept_id = result[0].id;
+                db.query(`INSERT INTO role (title, salary, department_id) VALUE("${title}","${salary}", ${dept_id})`, (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        console.log(`Added ${title} to role db`);
+                        resolve();
+
+                    }
+                })
+
+            }
+        })
+    })
+};
 // Exporting to index.js
-module.exports = { displayDep, displayRoles, displayEmployees };
+module.exports = { displayDep, displayRoles, displayEmployees, getEmployeeNames, getRoleTitles, getDepartmentNames, addEmployee, addRole };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const SQL = require('./helpers/sqlRequest.js');
 
-var menuQuestion = [
+const menuQuestion = [
     {
         // type, message, name, choices
         type: 'list',
@@ -11,10 +11,12 @@ var menuQuestion = [
     }
 ];
 
+// TO DO - CASES: add case for "dept," add case for "update employee role"
+// Display Menu Choices.
 function displayMenu() {
     inquirer.prompt(menuQuestion).then((answers) => {
         console.log('This is the answer variable: ', answers);
-        // switch - tests what we're answering
+        // Switch tests what we're answering. gets answer from the array stored in menuQuestion.
         switch (answers.menu) {
             case 'View all departments':
                 SQL.displayDep().then(() => {
@@ -30,10 +32,171 @@ function displayMenu() {
                 SQL.displayEmployees().then(() => {
                     displayMenu();
                 });
+                break;
+            case 'Add employee':
+                // in the same file
+                displayEmpQuestions();
+                break;
+            case 'Add role':
+                // in the same file
+                displayRoleQuestions();
+                break;
+            case 'Add department':
+                displayDeptQuestions();
+                break;
+            case 'Update employee role':
+                updateEmpRole();
+                break;
+            case 'quit':
+                break;
+
         }
     });
 };
 
-// displaying table data with the functions here.
+// Display Employee Questions
+function displayEmpQuestions() {
+    var roleList = [];
+    var managerList = [];
+    SQL.getRoleTitles().then((result) => {
+        roleList = result;
+        SQL.getEmployeeNames().then((result) => {
+            managerList = result;
+            const newEmployeeQuestions = [
+                {
+                    type: 'input',
+                    message: "What is the employee's first name?",
+                    name: 'first_name',
+                },
+                {
+                    type: 'input',
+                    message: "What is the employee's last name?",
+                    name: 'last_name',
+                },
+                {
+                    type: 'list',
+                    message: "What is the employee's role?",
+                    name: 'role',
+                    choices: roleList
+                },
+                {
+                    type: 'list',
+                    message: "Who is the employee's manager?",
+                    name: 'manager',
+                    choices: managerList
+                }
+            ];
+            // asks the questions. pass in newEmployeeQuestions
+            // callback function 'data' has the answers to the questions
+            inquirer.prompt(newEmployeeQuestions).then((data) => {
+                SQL.addEmployee(data.first_name, data.last_name, data.role, data.manager).then(() => {
+                    // Goes and displays the menu questions again. This goes at the end of everything.
+                    displayMenu();
+                })
+            })
+        });
+    });
+};
 
+// Display role questions
+function displayRoleQuestions() {
+    // need 1 list
+    var deptList = [];
+    SQL.getDepartmentNames().then((result) => {
+        deptList = result;
+
+        const newRoleQuestions = [
+            {
+                type: 'input',
+                message: "What is the employee's first name?",
+                name: 'first_name',
+            },
+            {
+                type: 'input',
+                message: "What is the employee's last name?",
+                name: 'last_name',
+            },
+            {
+                type: 'list',
+                message: "What is the employee's role?",
+                name: 'role',
+                choices: roleList
+            },
+            {
+                type: 'list',
+                message: "Who is the employee's manager?",
+                name: 'manager',
+                choices: managerList
+            }
+        ];
+        // asks the questions. pass in newEmployeeQuestions
+        // callback function 'data' has the answers to the questions
+        inquirer.prompt(newRoleQuestions).then((data) => {
+            SQL.addEmployee(data.first_name, data.last_name, data.role, data.manager).then(() => {
+                // Goes and displays the menu questions again. This goes at the end of everything.
+                displayMenu();
+            })
+        })
+    });
+};
+
+// Display department questions
+function displayDeptQuestions() {
+    var deptQuestions = [];
+    SQL.displayDep().then((result) => {
+        deptQuestions = result;
+        const displayDepartmentQuestions = [
+            {
+                type: 'input',
+                message: "Enter the department you want to add: ",
+                name: 'dept_name',
+            }
+        ];
+        // asks the questions. pass in newEmployeeQuestions
+        // callback function 'data' has the answers to the questions
+        inquirer.prompt(displayDepartmentQuestions).then((data) => {
+            SQL.addEmployee(data.first_name, data.last_name, data.role, data.manager).then(() => {
+                // Goes and displays the menu questions again. This goes at the end of everything.
+                displayMenu();
+            })
+        });
+    });
+};
+
+
+//  please continue with this one 6/19/23 tuesday - Juan delgado
+function updateEmpRole() {
+    var displayAllEmployees = [];
+    var displayAllRoles = [];
+    // need query. need list of all roles
+    // which employee to update--needs list of all employees.
+    SQL.getEmployeeNames().then((result) => {
+        displayAllEmployees = result;
+        SQL.getRoleTitles().then((result) => {
+            displayAllRoles = result;
+            const questions = [{
+                type: 'list',
+                message: 'Select an employee whose role you want to update: ',
+                name: 'employeeList',
+                choices: displayAllEmployees,
+            },
+            {
+                type: 'list',
+                message: 'What is the new role for the employee? ',
+                name: 'roleList',
+                choices: displayAllRoles,
+            }
+            ];
+            inquirer.prompt(questions).then((data) => {
+                console.log(data);
+                UPDATE data.employeeList;
+                SETasdf;
+                WEB asdf;
+
+            })
+        });
+    });
+};
+
+// displaying table data with the functions here.
 displayMenu();
